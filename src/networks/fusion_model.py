0a2c2588ff0fbd083d9d2cddfde8727140e9bcb3
xiegeo <george@xiegeo.com>
2024-06-25 19:24:47 +0800

    vqa: debug mixed device

--- src/networks/fusion_model.py
+++ src/networks/fusion_model.py
@@ -25,12 +25,15 @@ class LinearFusionModel(nn.Module):
     def __init__(self, base_model:PCME):
         super(LinearFusionModel, self).__init__()
         self.base_model = base_model
+        device = next(self.base_model.parameters()).device 
         self.fc = nn.Linear(base_model.embed_dim *2 , base_model.embed_dim)
+        self.to(device)
+        print(f"LinearFusionModel device {device}")
     
     def forward(self, images, sentences, captions_word, lengths):
         device = next(self.base_model.parameters()).device  # Get the device of the model
-        print(f"LinearFusionModel device {device}")
-        print(f"  images device {images.device}")
+        #print(f"LinearFusionModel device {device}")
+        #print(f"  images device {images.device}")
 
         #images = images.to(device)
         #sentences = [sentence.to(device) for sentence in sentences] if sentences else []
@@ -39,9 +42,9 @@ class LinearFusionModel(nn.Module):
 
         outputs = self.base_model.forward(images, sentences, captions_word, lengths)
         image_features = outputs['image_features']
-        print(f"  image_features device {image_features.device}")
+        #print(f"  image_features device {image_features.device}")
         caption_features = outputs['caption_features']
-        print(f"  caption_features device {caption_features.device}")
+        #print(f"  caption_features device {caption_features.device}")
         fused_features = torch.cat((image_features, caption_features), dim=1)
         output = self.fc(fused_features)
         return output
